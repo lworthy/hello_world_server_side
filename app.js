54fcd1dccbf319219b50cc8cf27bb18d2b6a2e4f
Landen Worthy <landenow04@gmail.com>
2025-03-19 15:04:27 -0400

    Hello server world

---
+++ app.js
@@ -0,0 +1,11 @@
+var http = require("http");
+const port = process.env.PORT || 3001;
+
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Hello World!");
+  })
+  .listen(port);
+
+console.log(`Server running at http://localhost:${port}`);
